b91027b5bac48aaf324cc8c9ac4ea7d288f81edc
Joacim Olsson <Joacim.olsson95@gmail.com>
2022-03-26 15:31:47 +0100

    change salt

--- TaskTimeManage.Core/Security/Cryptography.cs
+++ TaskTimeManage.Core/Security/Cryptography.cs
@@ -14,19 +14,23 @@ namespace TaskTimeManage.Core.Security
             {
                 Rfc2898DeriveBytes pdb = new(EncryptionKey, new byte[]
                 {
-                    0x49,
-                    0x76,
-                    0x61,
-                    0x6e,
+                    0x65,
                     0x20,
-                    0x4d,
+                    0x61,
+                    0x76,
+                    0x49,
                     0x65,
-                    0x64,
                     0x76,
                     0x65,
+                    0x6e,
+                    0x76,
+                    0x4d,
                     0x64,
+                    0x64,
+                    0x49,
                     0x65,
-                    0x76
+                    0x76,
+                    0x65,
                 });
                 encryptor.Key = pdb.GetBytes(32);
                 encryptor.IV = pdb.GetBytes(16);
