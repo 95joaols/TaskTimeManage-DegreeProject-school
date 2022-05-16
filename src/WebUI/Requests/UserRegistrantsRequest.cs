77b1443f11c44346b5f818d394b3c0214f732b48
95joaols <95joaols@skola.boras.se>
2022-05-16 11:21:21 +0200

    add PasswordNotSame

---
+++ src/WebUI/Requests/UserRegistrantsRequest.cs
@@ -0,0 +1,17 @@
+﻿namespace WebUI.Requests;
+
+public class UserRegistrantsRequest
+{
+	public string Username
+	{
+		get; set;
+	}
+	public string Password
+	{
+		get; set;
+	}
+	public string RepeatPassword
+	{
+		get; set;
+	}
+}
